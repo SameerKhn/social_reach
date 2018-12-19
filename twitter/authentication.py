489c0e2321f5724f2b93735e31ff14f64934a3b0
SAMEER-KHAN\Sameer Khan <111@gmail.com>
2018-12-19 16:14:36 +0500

    7th Commit: Successfully retrieved hashtags and their count as well as done the percentages of hashtags total and elite percentages

--- twitter/authentication.py
+++ twitter/authentication.py
@@ -2,7 +2,6 @@ from tweepy import API
 from tweepy import OAuthHandler
 
 
-
 def get_twitter_auth():
 
   try:
@@ -26,7 +25,27 @@ def get_hashtags(tweet):
     hashtags = entities.get('hashtags', [])
     return [tag['text'].lower() for tag in hashtags]
 
+def NoOfHashtags (noOfTags , hashtagCount):
+    percentEliteList = []
+    tweetsWithHashtags= sum([count for noOfTags, count in hashtagCount.items() if noOfTags > 0])
+    tweetsNoOfHashtags = hashtagCount[0]
+    tweetsTotal = tweetsNoOfHashtags + tweetsWithHashtags
+    tweets_with_hashtags_percent  = "%.2f" % (tweetsWithHashtags / tweetsTotal*100)
+    tweetsNo_Of_Hashtags_with_percent = "%.2f" % (tweetsNoOfHashtags / tweetsTotal*100)
+
+    print ("{} tweets without hashtags ({}%)".format(tweetsNoOfHashtags,tweetsNo_Of_Hashtags_with_percent))
+    print("{} tweets with atleast one hashtag ({}%)".format(tweetsWithHashtags, tweets_with_hashtags_percent))
+    # Dict = {'tweetsNoOfHashtags':tweetsNoOfHashtags,'tweetsNo_Of_Hashtags_with_percent':tweetsNo_Of_Hashtags_with_percent,
+    #         'tweetsWithHashtags':tweetsWithHashtags, 'tweets_with_hashtags_percent':tweets_with_hashtags_percent,
+    #         'hashtagCount':hashtagCount,'tweetsTotal':tweetsTotal }
 
 
+    for tag_count, tweet_count in hashtagCount.items():
+        if tag_count > 0:
+            percent_total = "%.2f" % (tweet_count / tweetsTotal * 100)
+            percent_elite = "%.2f" % (tweet_count / tweetsWithHashtags * 100)
+            #print("{} tweets with {} hashtags ({}% total, {}% elite)".format(tweet_count, tag_count,percent_total,percent_elite))
+            tempList=[tweet_count,tag_count,percent_total,percent_elite]
+            percentEliteList.append(tempList)
 
-
+    return (tweetsNoOfHashtags, tweetsNo_Of_Hashtags_with_percent, tweetsWithHashtags, tweets_with_hashtags_percent,percentEliteList)
--- twitter/views.py
+++ twitter/views.py
@@ -3,15 +3,21 @@ from tweepy import Cursor
 from . import authentication
 import json
 from collections import Counter
+from collections import defaultdict
 
 # Create your views here.
+
+
 
 def index(request):
     client = authentication.get_twitter_client()
     list=[]
     hashtagList=[]
     hashtags = Counter()
-    user ='ZAbbasOfficial'
+    #Dictionary={}
+    percentEliteList=[]
+    user ='PacktPub'
+    hashtag_count = defaultdict(int)
 
     for page in Cursor(client.user_timeline, screen_name=user, count=200).pages(3):
        for status in page:
@@ -21,9 +27,16 @@ def index(request):
         tweet = json.loads(line)
         hashtagsInTweet = authentication.get_hashtags(tweet)
         hashtags.update(hashtagsInTweet)
+        n_of_hashtags = len(hashtagsInTweet)
+        hashtag_count[n_of_hashtags] += 1
 
     for tag in hashtags.most_common(20):
         hashtagList.append(tag)
 
-    context = {'status': hashtagList}
+    # Dictionary = authentication.NoOfHashtags(list,hashtagsInTweet)
+    #hashtagPercentListElite = authentication.HashtagsWithPercentElite()
+    tweetsNoOfHashtags, tweetsNo_Of_Hashtags_with_percent, tweetsWithHashtags,tweets_with_hashtags_percent,percentEliteList=authentication.NoOfHashtags(n_of_hashtags,hashtag_count)
+
+    context = {'status': hashtagList, 'tweetsNoOfHashtags':tweetsNoOfHashtags,'tweetsNo_Of_Hashtags_with_percent':tweetsNo_Of_Hashtags_with_percent,
+               'tweetsWithHashtags':tweetsWithHashtags, 'tweets_with_hashtags_percent':tweets_with_hashtags_percent,'percentEliteList':percentEliteList}
     return render(request, 'twitter/index.html', context)
